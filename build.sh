833fe10e9086945326037545dfa9ede21b7c898d
kuldip424 <kuldipchuhan424@gmail.com>
2026-05-01 12:03:30 +0530

    debug: show migration status

--- build.sh
+++ build.sh
@@ -7,10 +7,4 @@ pip install -r requirements.txt
 echo "Running Collectstatic..."
 python manage.py collectstatic --no-input
 
-echo "Checking Migration Status..."
-python manage.py showmigrations
-
-echo "Running Migrations..."
-python manage.py migrate --noinput
-
 echo "Build Process Completed!"
